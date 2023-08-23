81194e713412f95e2a4f3897671a91328b1e7d11
ChirunoC9 <ciel_katana@outlook.com>
2023-08-23 15:23:35 +0800

    杂项-珂朵莉树 map 实现版本更新

---
+++ 杂项/珂朵莉树/chtholly_tree.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+
+template <typename __type>
+class chtholly_tree {
+private:
+    std::map<int, __type> mp;
+
+public:
+    using type = __type;
+
+public:
+    explicit chtholly_tree(int _first, const __type &__init_value)
+        : mp{{_first - 1, __init_value}} {
+        
+    }
+
+    void reset(int _first, const __type &__init_value) {
+        mp.clear();
+        mp[_first - 1] = __init_value;
+    }
+
+    void split(int pos) {
+        auto it = std::prev(mp.upper_bound(pos));
+        mp[pos] = it->second;
+    }
+
+    void split(int pl, int pr) {
+        split(pl);
+        split(pr);
+    }
+
+    auto get_pos_iter(int pos) {
+        return mp.find(pos);
+    }
+
+    void assign(int l, int r, const __type &value) {
+        split(l, r);
+        auto it = get_pos_iter(l);
+        while (it->first != r) 
+            it = mp.erase(it);
+        mp[l] = value;
+    }
+};
