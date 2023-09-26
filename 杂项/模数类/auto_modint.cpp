32048bf25b94bd0cc27122272b981dd7c737af86
ChirunoC9 <ciel_katana@outlook.com>
2023-09-26 10:13:46 +0800

    feat(移除auto_modint的命名空间): 移除auto_moint所在的命名空间

--- 杂项/模数类/auto_modint.cpp
+++ 杂项/模数类/auto_modint.cpp
@@ -3,234 +3,232 @@
 
 #include <iostream>
 
-namespace Leazozi::Type {
-    template <std::integral auto _P>
-        requires(_P >= 1)
-    struct ModInt {
-    private:
-        using u32 = uint32_t;
-        using i32 = int32_t;
-        using u64 = uint64_t;
-        using i64 = int64_t;
-        using i128 = __int128_t;
-        using u128 = __uint128_t;
+template <std::integral auto _P>
+    requires(_P >= 1)
+struct ModInt {
+private:
+    using u32 = uint32_t;
+    using i32 = int32_t;
+    using u64 = uint64_t;
+    using i64 = int64_t;
+    using i128 = __int128_t;
+    using u128 = __uint128_t;
 
-    public:
-        using value_type = decltype(_P);
+public:
+    using value_type = decltype(_P);
 
-    private:
-        value_type _value;
+private:
+    value_type _value;
 
-    public:
-        inline static constexpr auto P = _P;
+public:
+    inline static constexpr auto P = _P;
 
-    public:
-        constexpr ModInt() noexcept : _value(0) {}
+public:
+    constexpr ModInt() noexcept : _value(0) {}
 
-        constexpr ModInt(std::integral auto value) noexcept {
-            _value = value % P;
-            if (_value < 0)
-                _value += P;
+    constexpr ModInt(std::integral auto value) noexcept {
+        _value = value % P;
+        if (_value < 0)
+            _value += P;
+    }
+
+    explicit constexpr operator int() const noexcept {
+        return _value;
+    }
+
+    explicit constexpr operator long long() const noexcept {
+        return _value;
+    }
+
+    explicit constexpr operator bool() const noexcept {
+        return _value > 0;
+    }
+
+    constexpr auto operator==(const ModInt &other) const noexcept -> bool {
+        return _value == other._value;
+    }
+
+    constexpr auto operator!=(const ModInt &other) const noexcept -> bool {
+        return _value != other._value;
+    }
+
+    constexpr auto operator-() const noexcept -> ModInt {
+        return ModInt{_value ? P - _value : 0};
+    }
+
+    constexpr auto operator+() const noexcept -> ModInt {
+        return *this;
+    }
+
+    constexpr auto operator++() noexcept -> ModInt & {
+        ++_value;
+        if (_value == P)
+            _value = 0;
+        return *this;
+    }
+
+    constexpr auto operator--() noexcept -> ModInt & {
+        if (_value == 0)
+            _value = P;
+        --_value;
+        return *this;
+    }
+
+    constexpr auto operator++(int) noexcept -> ModInt {
+        auto prev = *this;
+        ++*this;
+        return prev;
+    }
+
+    constexpr auto operator--(int) noexcept -> ModInt {
+        auto prev = *this;
+        --*this;
+        return prev;
+    }
+
+    constexpr auto operator+=(const ModInt &other) noexcept -> ModInt & {
+        _value += other._value;
+        if (_value >= P)
+            _value -= P;
+        return *this;
+    }
+
+    constexpr auto operator+(const ModInt &other) const noexcept -> ModInt {
+        auto result = *this;
+        result += other;
+        return result;
+    }
+
+    constexpr auto operator-=(const ModInt &other) noexcept -> ModInt & {
+        _value -= other._value;
+        if (_value < 0)
+            _value += P;
+        return *this;
+    }
+
+    constexpr auto operator-(const ModInt &other) const noexcept -> ModInt {
+        auto result = *this;
+        result -= other;
+        return result;
+    }
+
+    constexpr auto operator*=(const ModInt &other) noexcept -> ModInt & {
+        if constexpr (std::is_same_v<value_type, i32> || std::is_same_v<value_type, int> ||
+                      std::is_same_v<value_type, u32>) {
+            _value = (value_type)(((u64)_value * other._value) % P);
+        } else if (std::is_same_v<value_type, i64> || std::is_same_v<value_type, u64> ||
+                   std::is_same_v<value_type, long long>) {
+            _value = (value_type)((u128)_value * other._value) % P;
+        } else {
+            _value = _SlowMut(_value, other._value);
         }
+        return *this;
+    }
 
-        explicit constexpr operator int() const noexcept {
-            return _value;
-        }
+    constexpr auto operator*(const ModInt &other) const noexcept -> ModInt {
+        auto result = *this;
+        result *= other;
+        return result;
+    }
 
-        explicit constexpr operator long long() const noexcept {
-            return _value;
-        }
+    constexpr auto operator^(std::integral auto i) const noexcept -> ModInt {
+        return _Pow(*this, i);
+    }
 
-        explicit constexpr operator bool() const noexcept {
-            return _value > 0;
-        }
+    constexpr auto operator^=(std::integral auto i) const noexcept -> ModInt & {
+        *this = (*this) ^ i;
+        return *this;
+    }
 
-        constexpr auto operator==(const ModInt &other) const noexcept -> bool {
-            return _value == other._value;
-        }
+    constexpr auto operator/=(const ModInt &other) noexcept -> ModInt & {
+        return (*this) *= other.GetInv();
+    }
 
-        constexpr auto operator!=(const ModInt &other) const noexcept -> bool {
-            return _value != other._value;
-        }
+    constexpr auto operator/(const ModInt &other) const noexcept -> ModInt {
+        auto result = *this;
+        result /= other;
+        return result;
+    }
 
-        constexpr auto operator-() const noexcept -> ModInt {
-            return ModInt{_value ? P - _value : 0};
-        }
+    constexpr auto GetInv() const noexcept -> ModInt {
+        return (*this) ^ (P - 2);
+    }
 
-        constexpr auto operator+() const noexcept -> ModInt {
-            return *this;
-        }
+    constexpr auto GetValue() const noexcept -> value_type {
+        return _value;
+    }
 
-        constexpr auto operator++() noexcept -> ModInt & {
-            ++_value;
-            if (_value == P)
-                _value = 0;
-            return *this;
-        }
+    friend constexpr auto operator+(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
+        return a + ModInt(b);
+    }
 
-        constexpr auto operator--() noexcept -> ModInt & {
-            if (_value == 0)
-                _value = P;
-            --_value;
-            return *this;
-        }
+    friend constexpr auto operator+(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
+        return ModInt(a) + b;
+    }
 
-        constexpr auto operator++(int) noexcept -> ModInt {
-            auto prev = *this;
-            ++*this;
-            return prev;
-        }
+    friend constexpr auto operator-(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
+        return a - ModInt(b);
+    }
 
-        constexpr auto operator--(int) noexcept -> ModInt {
-            auto prev = *this;
-            --*this;
-            return prev;
-        }
+    friend constexpr auto operator-(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
+        return ModInt(a) - b;
+    }
 
-        constexpr auto operator+=(const ModInt &other) noexcept -> ModInt & {
-            _value += other._value;
-            if (_value >= P)
-                _value -= P;
-            return *this;
-        }
+    friend constexpr auto operator*(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
+        return a * ModInt(b);
+    }
 
-        constexpr auto operator+(const ModInt &other) const noexcept -> ModInt {
-            auto result = *this;
-            result += other;
-            return result;
-        }
+    friend constexpr auto operator*(const std::integral auto &a, const ModInt b) noexcept -> ModInt {
+        return ModInt(a) * b;
+    }
 
-        constexpr auto operator-=(const ModInt &other) noexcept -> ModInt & {
-            _value -= other._value;
-            if (_value < 0)
-                _value += P;
-            return *this;
-        }
+    friend constexpr auto operator/(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
+        return a * ModInt(b).GetInv();
+    }
 
-        constexpr auto operator-(const ModInt &other) const noexcept -> ModInt {
-            auto result = *this;
-            result -= other;
-            return result;
-        }
+    friend constexpr auto operator/(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
+        return ModInt(a) * b.GetInv();
+    }
 
-        constexpr auto operator*=(const ModInt &other) noexcept -> ModInt & {
-            if constexpr (std::is_same_v<value_type, i32> || std::is_same_v<value_type, int> ||
-                          std::is_same_v<value_type, u32>) {
-                _value = (value_type)(((u64)_value * other._value) % P);
-            } else if (std::is_same_v<value_type, i64> || std::is_same_v<value_type, u64> ||
-                       std::is_same_v<value_type, long long>) {
-                _value = (value_type)((u128)_value * other._value) % P;
-            } else {
-                _value = _SlowMut(_value, other._value);
+    constexpr auto operator~() const noexcept -> ModInt {
+        return GetInv();
+    }
+
+    friend auto operator>>(std::istream &in, ModInt &x) -> std::istream & {
+        value_type d;
+        in >> d;
+        x = ModInt(d);
+        return in;
+    }
+
+    friend auto operator<<(std::ostream &out, const ModInt &x) -> std::ostream & {
+        out << x._value;
+        return out;
+    }
+
+private:
+    inline static constexpr auto _Pow(ModInt a, std::integral auto i) noexcept -> ModInt {
+        ModInt result = 1;
+        while (i > 0) {
+            if (i & 1) {
+                result *= a;
             }
-            return *this;
+            a *= a;
+            i >>= 1;
         }
+        return result;
+    }
 
-        constexpr auto operator*(const ModInt &other) const noexcept -> ModInt {
-            auto result = *this;
-            result *= other;
-            return result;
-        }
-
-        constexpr auto operator^(std::integral auto i) const noexcept -> ModInt {
-            return _Pow(*this, i);
-        }
-
-        constexpr auto operator^=(std::integral auto i) const noexcept -> ModInt & {
-            *this = (*this) ^ i;
-            return *this;
-        }
-
-        constexpr auto operator/=(const ModInt &other) noexcept -> ModInt & {
-            return (*this) *= other.GetInv();
-        }
-
-        constexpr auto operator/(const ModInt &other) const noexcept -> ModInt {
-            auto result = *this;
-            result /= other;
-            return result;
-        }
-
-        constexpr auto GetInv() const noexcept -> ModInt {
-            return (*this) ^ (P - 2);
-        }
-
-        constexpr auto GetValue() const noexcept -> value_type {
-            return _value;
-        }
-
-        friend constexpr auto operator+(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
-            return a + ModInt(b);
-        }
-
-        friend constexpr auto operator+(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
-            return ModInt(a) + b;
-        }
-
-        friend constexpr auto operator-(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
-            return a - ModInt(b);
-        }
-
-        friend constexpr auto operator-(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
-            return ModInt(a) - b;
-        }
-
-        friend constexpr auto operator*(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
-            return a * ModInt(b);
-        }
-
-        friend constexpr auto operator*(const std::integral auto &a, const ModInt b) noexcept -> ModInt {
-            return ModInt(a) * b;
-        }
-
-        friend constexpr auto operator/(const ModInt &a, const std::integral auto &b) noexcept -> ModInt {
-            return a * ModInt(b).GetInv();
-        }
-
-        friend constexpr auto operator/(const std::integral auto &a, const ModInt &b) noexcept -> ModInt {
-            return ModInt(a) * b.GetInv();
-        }
-
-        constexpr auto operator~() const noexcept -> ModInt {
-            return GetInv();
-        }
-
-        friend auto operator>>(std::istream &in, ModInt &x) -> std::istream & {
-            value_type d;
-            in >> d;
-            x = ModInt(d);
-            return in;
-        }
-
-        friend auto operator<<(std::ostream &out, const ModInt &x) -> std::ostream & {
-            out << x._value;
-            return out;
-        }
-
-    private:
-        inline static constexpr auto _Pow(ModInt a, std::integral auto i) noexcept -> ModInt {
-            ModInt result = 1;
-            while (i > 0) {
-                if (i & 1) {
-                    result *= a;
-                }
-                a *= a;
-                i >>= 1;
+    inline static constexpr auto _SlowMut(value_type a, std::integral auto i) noexcept -> value_type {
+        value_type result = 1 % P;
+        a %= P;
+        while (i > 0) {
+            if (i & 1) {
+                (result += a) %= P;
             }
-            return result;
+            (a += a) %= P;
+            i >>= 1;
         }
-
-        inline static constexpr auto _SlowMut(value_type a, std::integral auto i) noexcept -> value_type {
-            value_type result = 1 % P;
-            a %= P;
-            while (i > 0) {
-                if (i & 1) {
-                    (result += a) %= P;
-                }
-                (a += a) %= P;
-                i >>= 1;
-            }
-            return result;
-        }
-    };
-} // namespace Leazozi::Type
+        return result;
+    }
+};
