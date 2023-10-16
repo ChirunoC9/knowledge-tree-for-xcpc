044e25525c8b198e0b70a4e15da4f22f92c3e532
ChirunoC9 <ciel_katana@outlook.com>
2023-10-16 20:42:07 +0800

    feat(数据结构/树状数组/树状数组基础/树状数组.cpp): 更改代码风格,添加下标访问,将Update方法更名为Apply方法

--- 数据结构/树状数组/树状数组基础/树状数组.cpp
+++ 数据结构/树状数组/树状数组基础/树状数组.cpp
@@ -1,49 +1,64 @@
 #include <bits/stdc++.h>
 
 #if __cplusplus >= 202002L
-template <
-    typename Type,
-    std::regular_invocable<Type,Type> Operator = std::plus<Type>
->
+template <typename _Ty,
+          std::regular_invocable<_Ty, _Ty> _Operator = std::plus<_Ty>>
 #else
-template <
-    typename Type,
-    typename Operator = std::plus<Type>,
-    typename = std::enable_if<std::is_invocable<Operator,Type,Type>::value>::type
->
+template <typename Type, typename Operator = std::plus<Type>,
+          typename = std::enable_if<
+              std::is_invocable<Operator, Type, Type>::value>::type>
 #endif
 class FenwickTree {
 public:
-    explicit FenwickTree(int size, Type initValue = Type{}, Operator opt = Operator{}) 
-        : bit(size + 1, initValue), opt(opt) {}
-
-    auto Assign(int size, Type value) -> void {
-        bit.Assign(size, value);
-    }
-
-    auto Update(int idx, Type dx) -> void {
-        for (int i = idx + 1; i < (int)bit.size(); i += Lowbit(i)) {
-            bit[i] = opt(bit[i], dx);
-        }
-    }
-
-    auto Fill(Type value) -> void {
-        std::fill(bit.begin(), bit.end(), value);
-    }
-
-    auto Get(int idx, Type result = {}) const -> Type {
-        for (int i = idx + 1; i > 0; i -= Lowbit(i)) {
-            result = opt(result, bit[i]);
-        }
-        return result;
-    }
+    using value_type = _Ty;
+    using operator_type = _Operator;
 
 private:
-    constexpr static
-    auto Lowbit(int x) noexcept -> int {
+    inline constexpr static auto _Lowbit(int x) noexcept -> int {
         return x & -x;
     }
 
-    std::vector<Type> bit;
-    Operator opt;
+    std::vector<_Ty> _bit;
+    _Operator _opt;
+
+public:
+    explicit FenwickTree(int size, _Ty unit_val = _Ty{},
+                         _Operator opt = _Operator{})
+        : _bit(size + 1, unit_val), _opt(opt) {}
+
+    auto Assign(int size, _Ty unit_val) -> void {
+        _bit.Assign(size, unit_val);
+    }
+
+    auto Apply(int idx, _Ty dx) -> void {
+        for (int i = idx + 1; i < (int)_bit.size(); i += _Lowbit(i)) {
+            _bit[i] = _opt(_bit[i], dx);
+        }
+    }
+
+    auto Fill(_Ty unit_val) -> void {
+        std::fill(_bit.begin(), _bit.end(), unit_val);
+    }
+
+    auto Get(int idx, _Ty res = {}) const -> _Ty {
+        for (int i = idx + 1; i > 0; i -= _Lowbit(i)) {
+            res = _opt(res, _bit[i]);
+        }
+        return res;
+    }
+
+    auto Size() const noexcept -> std::size_t {
+        if (_bit.size() == 0)
+            return 0;
+        else
+            return _bit.size() - 1;
+    }
+
+    auto operator[](int idx) -> _Ty& {
+        return _bit[idx + 1];
+    }
+
+    auto operator[](int idx) const -> _Ty& {
+        return _bit[idx + 1];
+    }
 };
