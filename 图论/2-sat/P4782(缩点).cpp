cccc88e7d2803cf15243d628b9d6d03a26f58807
ChirunoC9 <ciel_katana@outlook.com>
2023-10-23 19:18:13 +0800

    feat(图论/2-sat/p4782(缩点).cpp): 更正准确实现的2-sat缩点实现

--- 图论/2-sat/P4782(缩点).cpp
+++ 图论/2-sat/P4782(缩点).cpp
@@ -1,32 +1,38 @@
-#include <bits/stdc++.h>
+#include <cmath>
+
+#include <iostream>
+#include <vector>
 
 int main() {
-    std::cin.tie(nullptr)->sync_with_stdio(false);
-    
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     int n, m;
     std::cin >> n >> m;
-    
-    std::vector adj(n * 2, std::vector<int>{});
-    for (int i = 0; i < m; i ++) {
-        int u, a, v, b;
-        std::cin >> u >> a >> v >> b;
-        u --; v --;
 
-        // a V b => not(a) -> b ^ not(b) -> a
-        adj[u * 2 + !a].emplace_back(v * 2 + b);
-        adj[v * 2 + !b].emplace_back(u * 2 + a);
+    // a -> b => not a V b
+    // 2 * u := not true; 2 * u + 1 := true
+    std::vector adj(n * 2, std::vector<int>{});
+    for (int i = 0; i < m; i += 1) {
+        int u, uw, v, vw;
+        std::cin >> u >> uw >> v >> vw;
+        u -= 1;
+        v -= 1;
+        adj[u * 2 + !uw].emplace_back(v * 2 + vw);
+        adj[v * 2 + !vw].emplace_back(u * 2 + uw);
     }
 
-    std::vector<int> dfn(n * 2, -1), low(n * 2, -1);
-    std::stack<int, std::vector<int>> stk;
-    std::vector<bool> inst(n * 2);
-    std::vector<int> scc_id(n * 2);
+    std::vector<int> dfn(n * 2, -1), low(dfn), scc_id(dfn);
     int scc_cnt = 0;
+    std::vector<int> stk;
+    std::vector<bool> inst(n * 2);
 
-    auto tarjan = [&, stamp{0}](auto &&self, int from) mutable -> void {
-        dfn[from] = low[from] = stamp ++;
-        stk.emplace(from);
+    auto trajan = [&, stamp{0}](auto &self, int from) mutable -> void {
+        dfn[from] = low[from] = stamp;
+        stamp += 1;
+        stk.emplace_back(from);
         inst[from] = true;
+
         for (auto to : adj[from]) {
             if (dfn[to] == -1) {
                 self(self, to);
@@ -35,35 +41,41 @@ int main() {
                 low[from] = std::min(low[from], dfn[to]);
             }
         }
+
         if (dfn[from] == low[from]) {
             int v;
             do {
-                v = stk.top();
-                stk.pop();
+                v = stk.back();
+                stk.pop_back();
                 inst[v] = false;
                 scc_id[v] = scc_cnt;
             } while (v != from);
-            scc_cnt ++;
+            scc_cnt += 1;
         }
     };
-    for (int i = 0; i < n * 2; i ++) {
+
+    for (int i = 0; i < n * 2; i += 1) {
         if (dfn[i] == -1) {
-            tarjan(tarjan, i);
+            trajan(trajan, i);
         }
     }
 
-    bool ok = true;
-    for (int i = 0; i < n && ok; i ++) {
-        ok &= scc_id[i * 2] != scc_id[i * 2 + 1];
-    }
-
-    if (!ok) {
-        std::cout << "IMPOSSIBLE\n";
-    } else {
-        std::cout << "POSSIBLE\n";
-        for (int i = 0; i < n; i ++) {
-            int ret = scc_id[i * 2] > scc_id[i * 2 + 1];  // 反着的拓扑序
-            std::cout << ret << " \n"[i + 1 == n];
+    // scc_id 是反的拓扑序
+    // 让拓扑序在后的节点作为选择结果
+    for (int i = 0; i < n; i += 1) {
+        if (scc_id[i * 2] == scc_id[i * 2 + 1]) {
+            std::cout << "IMPOSSIBLE\n";
+            return 0;
         }
     }
+    std::cout << "POSSIBLE\n";
+    for (int i = 0; i < n; i += 1) {
+        auto ret = scc_id[i * 2] < scc_id[i * 2 + 1];
+        if (ret) {
+            std::cout << 0 << ' ';
+        } else {
+            std::cout << 1 << ' ';
+        }
+    }
+    std::cout << '\n';
 }
