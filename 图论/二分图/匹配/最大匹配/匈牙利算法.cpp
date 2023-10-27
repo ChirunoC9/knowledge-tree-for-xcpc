781c6f7669630c5b4c5b449d8b6c4f4ed519ec44
ChirunoC9 <ciel_katana@outlook.com>
2023-10-27 21:28:30 +0800

    style(图论/二分图/匹配/最大匹配/匈牙利算法.cpp): 代码样式修改

--- 图论/二分图/匹配/最大匹配/匈牙利算法.cpp
+++ 图论/二分图/匹配/最大匹配/匈牙利算法.cpp
@@ -7,32 +7,33 @@ int main() {
     std::cin >> n >> m >> e;
 
     std::vector adj(n + m, std::vector<int>{});
-    for (int i = 0; i < e; i ++) {
+    for (int i = 0; i < e; i++) {
         int u, v;
         std::cin >> u >> v;
-        u --; v --;
+        u--;
+        v--;
         adj[u].emplace_back(v + n);
         adj[v + n].emplace_back(u);
     }
 
     std::vector<int> match(n + m, -1);
     std::vector<bool> vis(n + m);
-    
-    auto dfs = [&](auto &&dfs, int from) -> bool {
+
+    auto dfs = [&](auto &self, int from) -> bool {
         for (auto to : adj[from]) {
-            if (!vis[to]) {
-                vis[to] = true;
-                if (match[to] == -1 || dfs(dfs, match[to])) {
-                    match[to] = from;
-                    return true;
-                }
+            if (vis[to])
+                continue;
+            vis[to] = true;
+            if (match[to] == -1 || self(self, match[to])) {
+                match[to] = from;
+                return true;
             }
         }
         return false;
     };
 
     int cnt = 0;
-    for (int i = 0; i < n; i ++) {
+    for (int i = 0; i < n; i++) {
         std::fill(vis.begin(), vis.end(), false);
         cnt += dfs(dfs, i);
     }
